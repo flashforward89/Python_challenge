4e073a233f6343984810e494a9ed3da09e5254b2
FSR <your_email@fpshami@gmail.com>
2021-02-02 22:01:01 -0600

    Test with Ian

---
+++ PyBank/main.py
@@ -0,0 +1,26 @@
+#Your task is to create a Python script that analyzes the records to calculate each of the following:
+
+import os
+import csv
+
+# Path to collect data from the Resources folder
+budget_data_csv = os.path.join('..', 'Resources', 'budget_data.csv')
+
+#The total number of months included in the dataset
+def print_profitlosses(BankData):
+    Date = int(BankData [0])
+    ProfitLosses = int(BankData [1])
+len(Date)
+
+print ("Number of items in the list = ", len(Data))
+
+#The net total amount of "Profit/Losses" over the entire period
+
+
+#Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+
+
+#The greatest increase in profits (date and amount) over the entire period
+
+
+#The greatest decrease in losses (date and amount) over the entire period
