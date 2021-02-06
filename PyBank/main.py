5aa3206bd2ffac3058d1894566214d2f619879f2
FSR <your_email@fpshami@gmail.com>
2021-02-05 22:13:46 -0600

    V2

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,26 +1,77 @@
-#Your task is to create a Python script that analyzes the records to calculate each of the following:
+ #Your task is to create a Python script that analyzes the records to calculate each of the following:
 
+#Importing os and csv
 import os
 import csv
 
 # Path to collect data from the Resources folder
-budget_data_csv = os.path.join('..', 'Resources', 'budget_data.csv')
+budget_data_csv = os.path.join('Resources', 'budget_data.csv')
 
 #The total number of months included in the dataset
-def print_profitlosses(BankData):
-    Date = int(BankData [0])
-    ProfitLosses = int(BankData [1])
-len(Date)
 
-print ("Number of items in the list = ", len(Data))
+#opening the CSV file
+with open(budget_data_csv) as csvfile:
+    csvreader =csv.reader(csvfile, delimiter=',')
+    
+    csv_header=next(csvreader)
 
-#The net total amount of "Profit/Losses" over the entire period
+#Defining lists and variables
+count =0
+profit = []
+totalprofit = 0
+monthlychange = []
+date = []
+initialprofit = 0
+totalchangeinprofits=0        
 
 
-#Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+# #The net total amount of "Profit/Losses" over the entire period
 
+# #Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
 
-#The greatest increase in profits (date and amount) over the entire period
+# #The greatest increase in profits (date and amount) over the entire period
 
+# #The greatest decrease in losses (date and amount) over the entire period
 
-#The greatest decrease in losses (date and amount) over the entire period
+     # Conducting the ask
+for row in csvreader:    
+      # Use count to count the number months in this dataset
+      count = count + 1 
+      date.append(row[0])
+
+      # Calucalting total profits
+      profit.append(row[1])
+      totalprofit = totalprofit + int(row[1])
+
+      #Calculate the average change in profits from month to month. Then calulate the average change in profits
+      finalprofit = int(row[1])
+      monthlychangeprofits = finalprofit - initialprofit
+
+      #Creating a list for monthly changes
+      monthlychange.append(monthlychangeprofits)
+
+      totalchangeprofits = totalchangeprofits + monthlychangeprofits
+      initialprofit = finalprofit
+
+      #Average change in profits
+      averagechangeprofits = (totalchangeprofits/count)
+      
+      #Find the change in profits minimum and maximum
+      greatestincreaseprofits = max(monthlychange)
+      greatestdecreaseprofits = min(monthlychange)
+
+      increasedate = date[monthlychange.index(greatestincreaseprofits)]
+      decreasedate = date[monthlychange.index(greatestdecreaseprofits)]
+      
+    
+print("PyBank")
+   
+
+  
+
+with open('financial_analysis.txt', 'w') as text:
+  
+    text.write("PyBank"+ "\n")
+   
+ 
+   
